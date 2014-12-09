b3e030a3da952a70276a97b05d519d3dac95337c
Mike L <cisstudents@ATC203-M25.local>
2014-12-09 14:19:58 -0800

    Select destination from table view and have the route displayed

--- Mapper/mapper/mapper/MapViewController.swift
+++ Mapper/mapper/mapper/MapViewController.swift
@@ -50,11 +50,14 @@ UITableViewDataSource
     
     private func initDataSource_() {
         dataSrc_ = DataSource<Place>()
-        dataSrc_.save(Place(name: "Cafe Rouge at the Strand", address: "9 Villiers St London WC2N 6NA United Kingdom"))
-        dataSrc_.save(Place(name: "Theodore Bullfrog", address: "26 John Adam St London WC2N 6HL, United Kingdom"))
-        dataSrc_.save(Place(name: "Lupita", address: "13 Villiers St London WC2N 6ND United Kingdom"))
-        dataSrc_.save(Place(name: "The Ship and Shovell", address: "1-3 Craven Passage London WC2N 5PH United Kingdom"))
-        dataSrc_.save(Place(name: "The Vaults Restaurant", address: "The RSA 8 John Adam St WC2N 6EZ, United Kingdom"))
+        //dataSrc_.save(Place(name: "Cafe Rouge at the Strand", address: "9 Villiers St London WC2N 6NA United Kingdom"))
+        //dataSrc_.save(Place(name: "Theodore Bullfrog", address: "26 John Adam St London WC2N 6HL, United Kingdom"))
+        //dataSrc_.save(Place(name: "Lupita", address: "13 Villiers St London WC2N 6ND United Kingdom"))
+        //dataSrc_.save(Place(name: "The Ship and Shovell", address: "1-3 Craven Passage London WC2N 5PH United Kingdom"))
+        //dataSrc_.save(Place(name: "The Vaults Restaurant", address: "The RSA 8 John Adam St WC2N 6EZ, United Kingdom"))
+        dataSrc_.save(Place(name: "The Vaults Restaurant", address: "1340 Scott Street San Francisco, CA"))
+        dataSrc_.save(Place(name: "The Vaults Restaurant", address: "1000 Scott Street San Francisco, CA"))
+
     }
     
     private func geocode_(place:Place) {
@@ -79,7 +82,7 @@ UITableViewDataSource
                             subTitle: place.address,
                             coordinate: coord)
                         
-                        place.annotation = annotation
+                        place.placemark = (placemarks[0] as CLPlacemark)
                         
                         self.mapView
                             .addAnnotation(annotation, select:false)
@@ -125,11 +128,52 @@ UITableViewDataSource
         self.mapView.showsUserLocation = true
     }
     
+    private func showRoute_(origin: MKMapItem!, destination: MKMapItem!) {
+        
+        var directionRequest : MKDirectionsRequest = MKDirectionsRequest()
+        directionRequest.setSource(origin)
+        directionRequest.setDestination(destination)
+        
+        var direction : MKDirections = MKDirections(request: directionRequest)
+        
+        direction.calculateDirectionsWithCompletionHandler({
+            (response:MKDirectionsResponse!, routeError:NSError!) -> Void in
+            if (routeError != nil || response.routes.isEmpty) {
+                return
+            } else {
+                let route: MKRoute = response.routes[0] as MKRoute
+                
+                // clear previous route overlays
+                if (self.mapView.overlays != nil) {
+                    self.mapView.removeOverlays(self.mapView.overlays)
+                }
+                
+                self.mapView.addOverlay(route.polyline!)
+                
+                var rect : MKMapRect = route.polyline.boundingMapRect as MKMapRect
+                let spanX = 0.05
+                let spanY = 0.05
+                var newRegion = MKCoordinateRegion(center: self.mapView.userLocation.coordinate, span: MKCoordinateSpanMake(spanX, spanY))
+                self.mapView.setRegion(newRegion, animated: true)
+            }
+        })
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - MKMapViewDelegate
+    
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        let route: MKPolyline = overlay as MKPolyline
+        let routeRenderer = MKPolylineRenderer(polyline:route)
+        routeRenderer.lineWidth = 3.0
+        routeRenderer.strokeColor = UIColor.blueColor().colorWithAlphaComponent(0.5)
+        return routeRenderer
+    }
+    
     // MARK: - CLLocationManagerDelegate
     
     func locationManager(manager:CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
@@ -149,22 +193,27 @@ UITableViewDataSource
     func locationManager(manager:CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         if (status == .Authorized
             || status == .AuthorizedWhenInUse) {
-                locMgr_.desiredAccuracy = kCLLocationAccuracyThreeKilometers
-                locMgr_.startUpdatingLocation()
+            locMgr_.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+            locMgr_.startUpdatingLocation()
         }
     }
     
     func locationManager(manager:CLLocationManager!, didFailWithError error:NSError!) {
-        println(error)
+        println("[ERROR]: \(error)")
     }
     
     // MARK: - UITableViewDelegate
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
         var place:Place = dataSrc_.getAtIndex(indexPath.row)
+        
         self.mapView
-            .zoomToCoordinate(place.annotation!.coordinate)
             .selectAnnotation(place.annotation, animated: true)
+        
+        self.showRoute_(
+            MKMapItem.mapItemForCurrentLocation(),
+            destination: MKMapItem(placemark: MKPlacemark(placemark: place.placemark)))
     }
     
     // MARK: - UITableViewDataSource
--- Mapper/mapper/mapper/Place.swift
+++ Mapper/mapper/mapper/Place.swift
@@ -10,12 +10,27 @@ import MapKit
 
 class Place {
     
-    init(name:NSString, address:NSString, annotation:MKAnnotation? = nil) {
+    private var annotation_:MKAnnotation? = nil
+    
+    init(name:NSString, address:NSString, placemark:CLPlacemark? = nil) {
         self.name = name
         self.address = address
-        self.annotation = annotation
+        self.placemark = placemark
     }
+    
     var name:NSString
     var address:NSString
-    var annotation:MKAnnotation?
+    var placemark:CLPlacemark?
+    var annotation : MKAnnotation? {
+        get {
+            if (annotation_ == nil) {
+                var anno : MKPointAnnotation = MKPointAnnotation()
+                anno.title = self.name
+                anno.subtitle = self.address
+                anno.coordinate = (self.placemark?.location)!.coordinate
+                self.annotation_ = anno
+            }
+            return self.annotation_
+        }
+    }
 }
